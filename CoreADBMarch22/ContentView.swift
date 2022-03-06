e29ae990235cd6b2712caa8e198189506fb583b2
ADB <adbdev@icloud.com>
2022-03-06 08:54:39 -0600

    Initial Commit

---
+++ CoreADBMarch22/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreADBMarch22
+//
+//  Created by A Bridge on 3/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreADBMarch22/CoreADBMarch22App.swift
@@ -0,0 +1,17 @@
+//
+//  CoreADBMarch22App.swift
+//  CoreADBMarch22
+//
+//  Created by A Bridge on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreADBMarch22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
