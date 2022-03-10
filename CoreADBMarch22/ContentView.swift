b42b0e70dfacd81fd0a1ed7c5a9ccbef4c09e95c
ADB <adbdev@icloud.com>
2022-03-09 19:53:58 -0600

    Converted contenview to NavLink and added showitemview to show the list

--- CoreADBMarch22/ContentView.swift
+++ CoreADBMarch22/ContentView.swift
@@ -4,61 +4,37 @@
 //
 //  Created by A Bridge on 3/6/22.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @State private var sortDescriptors = [SortDescriptor<Singer>]()
-    @State private var sortme: SortOrder = .reverse
+    
+    @FetchRequest(
+        sortDescriptors: [],
+        animation: .default)
+    private var items: FetchedResults<Singer>
     
     var body: some View {
-        VStack {
-            FilteredList(sortDescriptors: sortDescriptors) { (singer: Singer) in
-                Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
-            }
-            
-            Button("Add") {
-                let taylor = Singer(context: moc)
-                taylor.firstName = "Taylor"
-                taylor.lastName = "Swift"
+        NavigationView {
+            Form {
+                Section {
+                NavigationLink(destination: AddItemView())
+                {
+                    Text("New Item")
+                }
+                }
                 
-                let ed = Singer(context: moc)
-                ed.firstName = "Ed"
-                ed.lastName = "Sheeran"
-                
-                let adele = Singer(context: moc)
-                adele.firstName = "Adele"
-                adele.lastName = "Adkins"
-                
-                try? moc.save()
-            }
-            
-            
-            Button("SortIt") {
-                toggleSortOrder()
-                sortDescriptors = [SortDescriptor(\.firstName, order: sortme )]
-                
-            }
-            
-            Button("Sort A-Z") {
-                sortDescriptors = [SortDescriptor(\.firstName)]
-            }
-            
-            Button("Sort Z-A") {
-                sortDescriptors = [SortDescriptor(\.firstName, order: .reverse)]
-            }
-            
-            Button("Sort Reset") {
-                sortDescriptors = [SortDescriptor<Singer>]()
-                print("toggleSortOrder: Sort Reset")
-            }
-        }
-    }
-    
-    private func toggleSortOrder() {
-        sortme = (sortme == .reverse ? .forward : .reverse)
-        print("toggleSortOrder: order = \(sortme)")
+               // Divider()
+                Section {
+                NavigationLink(destination: ShowItemsView())
+                {
+                    Text("Show \(items.count) Items")
+                    //Text("Show \(moc.Singer.count)")
+                    Text("Show Items")                }
+                }
+            }.navigationBarTitle("StuffCost")
+            //.navigationBarHidden(true)
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
--- CoreADBMarch22/DataControler.swift
+++ CoreADBMarch22/DataControler.swift
@@ -9,7 +9,7 @@ import CoreData
 import Foundation
 
 class DataController: ObservableObject {
-    let container = NSPersistentContainer(name: "CoreADBDynSort")
+    let container = NSPersistentContainer(name: "CoreTest32")
     
     init() {
         container.loadPersistentStores { description, error in
